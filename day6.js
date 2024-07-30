7ee40dbb8b4d5f1d4cebfbee42db8e585951326a
jainmayank12566 <jainmayank12566@gmail.com>
2024-07-30 13:59:01 +0530

    6-commit

---
+++ day6.js
@@ -0,0 +1,67 @@
+//task1
+// const arr=[1,2,3,4,5]
+// console.log(arr)
+
+//task2
+// const arr=[1,2,3,4,5];
+// console.log(arr[0])
+// console.log(arr[arr.length-1])
+
+//task3
+// const arr=[1,2,3,4,5];
+// arr.push(1)
+// console.log(arr)
+
+//task4
+// const arr=[1,2,3,4,5];
+// arr.pop();
+// console.log(arr)
+
+//task5
+// const arr=[1,2,3,4,5];
+// arr.unshift(5)
+// console.log(arr)
+
+//task6
+// const arr=[1,2,3,4,5];
+// arr.shift()
+// console.log(arr)
+
+//task7
+// const arr=[1,2,3,4,5];
+// const newarr=arr.map((val)=>val+val);
+// console.log(arr)
+// console.log(newarr)
+
+//task8
+// const arr=[1,2,3,4,5];
+// const newarr=arr.filter((val)=>val%2===0)
+// console.log(arr)
+// console.log(newarr)
+
+//task9
+// const arr=[1,2,3,4,5];
+// const newarr=arr.reduce((a,b)=>a+b,0)
+// console.log(arr)
+// console.log(newarr)
+
+//task10
+// const arr=[1,2,3,4,5];
+// for(let i=0; i<arr.length; i++){
+    // console.log(arr[i]);
+// }
+
+//task11
+// const arr=[1,2,3,4,5]
+// arr.forEach((val)=>{
+    // console.log(val)
+// })
+
+//task12
+// const arr=[[0,1],[1,2],[2,3],[3,4]];
+// console.log(arr);
+
+//task13
+const arr=[[0,1],[1,2],[2,3],[3,4]]
+console.log(arr[0])
+console.log(arr[0][1])
