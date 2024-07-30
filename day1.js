fcd2a3e60ffdd9daa88ef449fdaac3c82ee80c39
jainmayank12566 <jainmayank12566@gmail.com>
2024-07-30 12:45:37 +0530

    first-commit

---
+++ day1.js
@@ -0,0 +1,37 @@
+//task1
+var value=10;
+console.log(value);
+
+//task2
+let val="hello";
+console.log(val);
+
+//task3
+const a=true;
+console.log(a);
+
+//task4
+const x=1;
+const y="hello";
+const z=true;
+const obj={
+    name:"hello",
+    age:100
+}
+const arr=[1,2,3,4,5];
+console.log(typeof x)
+console.log(typeof y)
+console.log(typeof z)
+console.log(typeof obj)
+console.log(typeof arr)
+
+//task5
+let b="hello";
+console.log(b);
+b="world";
+console.log(b);
+
+//task6
+const data=1;
+data=2;//error comes assignment to constant variable
+console.log(data);
