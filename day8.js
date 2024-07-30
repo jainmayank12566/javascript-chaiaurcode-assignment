925a1dfa1ec4605626485736b6963dab0144e005
jainmayank12566 <jainmayank12566@gmail.com>
2024-07-30 17:10:10 +0530

    8-commit

---
+++ day8.js
@@ -0,0 +1,74 @@
+//task1
+// const name="hello-world";
+// const age=100;
+// const str=`${name}-${age}`;
+// console.log(str);
+
+//task2
+// const str=`hello how r you 
+// i am fine you`;
+// console.log(str);
+
+//task3
+// const arr=[1,2,3,4,5]
+// const[a,b]=arr;
+// console.log(arr)
+// console.log("a",a)
+// console.log("b",b)
+
+//task4
+// const book={
+    // author:"mr",
+    // title:"hello"
+// }
+// const {author,title}=book;
+// console.log(author)
+// console.log(title)
+
+//task5
+// const arr=[1,2,3,4,5];
+// const newarr=[...arr,5,4,3,2,1];
+// console.log(arr)
+// console.log(newarr)
+
+//task6
+// let sum=0;
+// function sayhello(...str){
+//     for(let i=0; i<str.length; i++){
+//         sum +=str[i];
+//     }
+//     return sum;
+// }
+// console.log(sayhello(1,2,3,4,5))
+
+//task7
+// function sayhello(a,b=1){
+//     return a*b
+// }
+// console.log(sayhello(1,2))
+// console.log(sayhello(1))
+
+//task8
+// const fullname="hello";
+// const age=100;
+// function sayhello(){
+//     console.log("sayhello")
+// }
+// const obj={
+//     fullname,
+//     age,
+//     sayhello
+// }
+
+//task9
+// const fullname="name";
+// const rollno="age";
+// const sayhello="welcome";
+// const obj={
+//     [fullname]:"helloworld",
+//     [rollno]:100,
+//     [sayhello](){
+//         console.log("sayhello")
+//     }
+// }
+// console.log(obj);
