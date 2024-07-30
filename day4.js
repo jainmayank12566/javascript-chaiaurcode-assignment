52a0af2f14381f3eb0ed21408c214225e12d9af8
jainmayank12566 <jainmayank12566@gmail.com>
2024-07-30 13:32:57 +0530

    fourth-commit

---
+++ day4.js
@@ -0,0 +1,73 @@
+//task1
+// for(let i=1; i<=10; i++){
+    // console.log(i);
+// }
+
+//task2
+// for(let i=1; i<=10; i++){
+    // console.log(5*i);
+// }
+
+//task3
+// let i=1;
+// let sum=0;
+// while(i<=10){
+    // sum +=i;
+    // i++;
+// }
+// console.log(sum)
+
+//task4
+// let i=10;
+// while(i>=1){
+    // console.log(i);
+    // i--;
+// }
+
+//task5
+// let i=1;
+// do{
+    // console.log(i);
+    // i++;
+// }
+// while(i<=5);
+
+//task6
+// let i=0;
+// let sum=1;
+// do{
+//     if(i===0){
+//         sum=1;
+//     }
+//     else{
+//         sum *=i;
+//         i--;
+//     }
+// }
+// while(i>0);
+// console.log(sum);
+
+//task7
+// for(let i=1; i<=5; i++){
+    // let sum="";
+    // for(let j=1; j<=i; j++){
+        // sum +="*";
+    // }
+    // console.log(sum);
+// }
+
+//task8
+// for(let i=1; i<=10; i++){
+//     if(i===5){
+//         continue;
+//     }
+//     console.log(i);
+// }
+
+//task9
+for(let i=1; i<=10; i++){
+    if(i===7){
+        break;
+    }
+    console.log(i);
+}
