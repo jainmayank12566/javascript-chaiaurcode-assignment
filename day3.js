7dc5b3d6cffe055164ac33bbd8c6de8441d19398
jainmayank12566 <jainmayank12566@gmail.com>
2024-07-30 13:13:42 +0530

    third-commit

---
+++ day3.js
@@ -0,0 +1,103 @@
+//task1
+// const value=10;
+// if(value>0){
+//     console.log("positive");
+// }
+// else{
+//     if(value===0){
+//         console.log("zero")
+//     }
+//     else{
+//         console.log("-ve")
+//     }
+// }
+
+//task2
+// const age=10;
+// if(age>=18){
+//     console.log("eligible")
+// }
+// else{
+//     console.log("not eligible")
+// }
+
+//task3
+// const a=10;
+// const b=5;
+// const c=2;
+// if(a>=b && a>=c){
+//     console.log("a",a)
+// }
+// else{
+//     if(b>=a && b>=c){
+//         console.log("b",b)
+//     }
+//     else{
+//         console.log("c",c)
+//     }
+// }
+
+//task4
+// const day=2;
+// switch(day){
+//     case 1:
+//         console.log("monday");
+//         break;
+//     case 2:
+//         console.log("tuesday");
+//         break;
+//     case 3:
+//         console.log("wednesday");
+//         break;
+//     case 4:
+//         console.log("thursday");
+//         break;
+//     case 5:
+//         console.log("friday");
+//         break;
+//     case 6:
+//         console.log("saturaday");
+//         break;
+//     case 7:
+//         console.log("sunday");
+//         break;
+//     default:
+//         console.log("enter a valis day");   
+// }
+
+//task5
+// let score=79;
+// let grade;
+// switch(true){
+//     case (score>90 && score<=100):
+//         grade='A';
+//         console.log(grade);
+//         break;
+//     case (score>80 && score<=90):
+//         grade='B';
+//         console.log(grade);
+//         break;
+//     case (score>70 && score<=80):
+//         grade='C';
+//         console.log(grade);
+//         break;
+//     case (score>60 && score<=70):
+//         grade='D';
+//         console.log(grade);
+//         break;
+//     default:
+//         console.log("fail")
+// }
+
+//task6
+// const value=9;
+// value%2===0?console.log("even"):console.log("odd")
+
+//task7
+// const year=2024;
+// if((year%4===0 && year%100!==0) || (year%400===0)){
+    // console.log("year is leap year",year);
+// }
+// else{
+    // console.log("year is not leap year",year);
+// }
