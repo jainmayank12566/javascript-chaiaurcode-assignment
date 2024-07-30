9f4851359f4ddfa2d3226f6cbc06fefd314a7f58
jainmayank12566 <jainmayank12566@gmail.com>
2024-07-30 17:27:46 +0530

    9-commit

---
+++ day9.js
@@ -0,0 +1,51 @@
+//task1
+// const h1=document.getElementById("h1");
+// h1.innerText="changed";
+
+//task2
+// const h1=document.getElementsByClassName("h1-class");
+// h1[0].style.backgroundColor="red";
+
+//task3
+// const body=document.querySelector("body");
+// const ele=document.createElement("div");
+// ele.innerText="div added body";
+// body.append(ele);
+
+// task4
+// const ul=document.querySelector("ul");
+// const ele=document.createElement("li");
+// ele.innerText="cherry";
+// ul.append(ele);
+
+//task5
+// const h1=document.getElementById("h1");
+// h1.remove();
+
+//task6
+// lastChild-it detect comment part,whitespaces,text,html element
+// lastElementChild-it detect html element only
+// const ul=document.querySelector("ul").lastElementChild;
+// ul.remove();
+
+//task7
+// const img=document.querySelector("img");
+// img.src="https://upload.wikimedia.org/wikipedia/commons/thumb/9/90/PM_at_the_Pran_Pratishtha_ceremony_of_Shree_Ram_Janmaboomi_Temple_in_Ayodhya%2C_Uttar_Pradesh_on_January_22%2C_2024_%28cropped%29.jpg/1200px-PM_at_the_Pran_Pratishtha_ceremony_of_Shree_Ram_Janmaboomi_Temple_in_Ayodhya%2C_Uttar_Pradesh_on_January_22%2C_2024_%28cropped%29.jpg";
+
+//task8
+// const h1=document.getElementById("h1");
+// h1.classList.add("red-color");
+// // h1.classList.remove("red-color")
+
+//task9
+// const p=document.querySelector("p")
+// const buttons=document.querySelector("button")
+// buttons.addEventListener("click",()=>{
+    // p.innerText="p changed";
+// })
+
+//task10
+const div=document.querySelector("div");
+div.addEventListener("mouseover",()=>{
+    div.style.borderColor="yellow";
+})
