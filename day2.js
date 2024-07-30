259240df45d557a51054c17d756b93f9a6b79adc
jainmayank12566 <jainmayank12566@gmail.com>
2024-07-30 13:00:19 +0530

    second-commit

---
+++ day2.js
@@ -0,0 +1,89 @@
+//task1
+// const a=5;
+// const b=2;
+// const result=a+b;
+// console.log(result);
+
+//task2
+// const a=5;
+// const b=2;
+// const sub=a-b;
+// console.log(sub);
+
+//task3
+// const a=5;
+// const b=2;
+// const mul=a*b;
+// console.log(mul);
+
+//task4
+// const a=5;
+// const b=2;
+// const div=a/b;
+// console.log(div);
+
+//task5
+// const a=5;
+// const b=2;
+// const rem=a%b;
+// console.log(rem);
+
+//task6
+// let value=10;
+// value +=10;
+// console.log(value);
+
+//task7
+// let value=10;
+// value -=5;
+// console.log(value);
+
+//task8
+// const val=5;
+// const newval=2;
+// const result=val>newval;
+// console.log(result);
+// const newresult=val<newval;
+// console.log(newresult);
+
+//task9
+// const val=10;
+// const newval=5;
+// const result=val>=newval;
+// console.log(result);
+// const newresult=val<=newval;
+// console.log(newresult);
+
+//task10
+// const val=10;
+// const newval="10";
+// console.log(val==newval);
+// console.log(val===newval);
+
+//task11
+// const val=10;
+// const newval=5;
+// if(val>=10 && newval<=10){
+//     console.log(true)
+// }
+// else{
+//     console.log(false);
+// }
+
+//task12
+// const val=10;
+// const newval=5;
+// if(val>=5 || newval<5){
+//     console.log(true)
+// }
+// else{
+//     console.log(false);
+// }
+
+//task13
+// const val=false;
+// console.log(!val)
+
+//task14
+// const value=10
+// value>=0?console.log("positive"):console.log("-ve");
