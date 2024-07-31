2c04d2d978beddd431cb3be6d5f694a581887d1b
jainmayank12566 <jainmayank12566@gmail.com>
2024-07-31 20:12:40 +0530

    13-commit

---
+++ day13/day13.js
@@ -0,0 +1,24 @@
+//task1
+// import {add} from './script13.js';
+// console.log(add(1,2));
+
+//task2
+// import {obj} from './script13.js';
+// console.log(obj);
+
+//task3
+// import {add,multiply} from './script13.js';
+// console.log(add(1,2))
+// console.log(multiply(1,2))
+
+//task4
+// import add from './script13.js';
+// console.log(add(1,2));
+
+//task5
+// import {add,value} from './script13.js';
+// console.log(add(1,2))
+// console.log(value)
+
+//task6 task7 done
+//task8 not done
---
+++ day13/script13.js
@@ -0,0 +1,34 @@
+//task1
+// export function add(a,b){
+    // return a+b;
+// }
+
+//task2
+// export const obj={
+    // name:"hello",
+    // age:100,
+    // sayhello(){
+        // return `${this.name}-${this.age}`;
+    // }
+// }
+
+//task3
+// function add(a,b){
+    // return a+b;
+// }
+// function multiply(a,b){
+    // return a*b;
+// }
+// export {add,multiply};
+
+//task4
+// export default function add(a,b){
+    // return a+b;
+// }
+
+//task5
+// function add(a,b){
+    // return a+b;
+// }
+// const value=100;
+// export {add,value}
