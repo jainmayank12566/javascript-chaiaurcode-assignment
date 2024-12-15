5a4e4a215a7802923ae424317a9b148a0ef45da3
jainmayank12566 <jainmayank12566@gmail.com>
2024-12-15 23:18:34 +0530

    update

--- day13/index.js
+++ day13/index.js
@@ -1,22 +1,22 @@
 //task1
-// import {add} from './script13.js';
+// import {add} from './script.js';
 // console.log(add(1,2));
 
 //task2
-// import {obj} from './script13.js';
+// import {obj} from './script.js';
 // console.log(obj);
 
 //task3
-// import {add,multiply} from './script13.js';
+// import {add,multiply} from './script.js';
 // console.log(add(1,2))
 // console.log(multiply(1,2))
 
 //task4
-// import add from './script13.js';
+// import add from './script.js';
 // console.log(add(1,2));
 
 //task5
-// import {add,value} from './script13.js';
+// import {add,value} from './script.js';
 // console.log(add(1,2))
 // console.log(value)
 
